78c81aaee52cb80ed48011c1515728c27d6041d2
suryawanshi55 <107236171+suryawanshi55@users.noreply.github.com>
2022-07-13 15:55:43 +0530

    Add files via upload

---
+++ summary1.py
@@ -0,0 +1,235 @@
+"___Python____"
+
+creator:Guido Van Rossum
+country:dutch
+he is still alive.
+DOB:31st jan.1956
+
+
+python was developed in Netherland.
+CWI:centrum wiskunde & informatica.
+Name python come from a T.V serial called Monty's python flying circus.
+type:comedy & circus.
+year:1969.
+seasons:4
+
+
+C language was developed by Dennis Ritchie in the year 1972.
+C language is considered as mother of all the programming languages.
+place:AT&T bell labs.
+AT&T :American telephone &telegraph company.
+
+
+Java Language was developed by James Goshling in the year 1995.
+
+
+
+python dates:
+python developed/implementation:Dec 1989.
+
+
+python software download:python.org
+version:3.10.4
+
+operation system:
+
+window
+linux
+Android
+Macos.
+Ubuntu.
+Kali linux.
+solaris.
+Raspberry.
+parrot os.
+
+
+how to check which version of python being used or installed on pc/laptop:
+1)in command prompt type= python --version (then enter).
+2)click window-control pannal-python 3.10.4
+
+python 3.9 version cannot be used on window 7 or earlior.
+
+
+BMP:bitmap image file,developed by microsoft corporation.
+
+IDLE:integrated development learning enviroment.(don't save code).
+IDE:integrated development enviroment/text editor.
+
+
+IDE/text editors for python.
+
+python
+notepad
+notepad++
+pycharm 
+Jupyter notebook
+spyder
+thonny
+subline
+vim editor
+microsoft visual studio code
+Atom
+Brackets.
+
+
+INTRODUCTION OF PYTHON:
+python is popular programming language.
+python can be used on a server to create web application.
+python is used for- web development(server-side),software development,mathematics,system scripting.
+python can be used to handle big data and perform complex mathematics.
+python is a interpreter system, meaning that code can be executed as soon as it is written.this means that prototyping can be very quick.
+every print statement takes a new line.
+
+
+COMMENTS IN PYTHON:
+comments hellps us to understand the line of code or programm.
+
+we have two types of comments:
+1)single line comment:
+it is represented by #.the shortcut for keeping or removing the singleline.
+comment is ctrl+/
+
+2)multiline comment:
+it is represented by three times single quotes or three time doubble quotes-(''' ''') or (""" """)
+
+
+PYTHON SYNTAX:
+Execute Python Syntax:
+python syntax can be executed by writing directly in the command line or by creating a python file on the server, using the .py file extension,and running it in the command line.
+
+Python Indentation:
+indentation refers to the spaces at the beginning of code line.where in other programming languages the indentationin code is for readability only, the indentationin python is very important.python uses indentation to indicate a block of code. python will give you an error if you skip the indentation.
+EXAMPLE:
+if 5 > 2:
+   print("five is greater than two!")
+syntax error:
+if 5 > 2:
+print("five is greater than two!")
+
+number of spaces is one or three.
+
+
+
+PYTHON VARIABLES:
+in python, varables are created when you assign a value to it.python allows you to assign value to multiple variables in one line.
+variables are containers for storing data values.
+you can get the data type of a variable with the type() function.you can reuse the variables.variables name are case-sensitive.
+
+Single or doubble quotes:
+string variable can be declared either by using single or double quotes.
+
+
+RULES OF A VARIABLE AND COMMENTS.
+
+1)a variable is mostly on leftside and right side also.
+2)variable are used for storing the value.
+3)variable works like containor.
+4)a variable should start with alphabet it can be lowercase or uppercase.
+5)python is case-senstitive.there is a d/b lower and uppercase letters.
+6)variable cannot contain keywords.
+7)A variable cannot start with numeric.
+8)a variable can start with underscore(_) symbol.
+9)except underscore other symbol are not allowed.
+10)a variable should not a space between them.
+
+Note:there is no limit for the variable name in python.
+
+
+PYTHON-OUTPUT VARIABLES:
+the python print() function is often used to output variables.
+ex:
+x="python is awesome"
+print(x)
+in the print() function, you output multiple variables,seperated by comma.
+ex:
+x="python"
+y="is"
+z="awesome"
+print(x,y,z)
+you can also use the +operator to output multiple variable:
+ex:
+x='python'
+y='is'
+z='awesome'
+print(x+y+z)
+
+
+DATA TYPES:
+variable can store data of different types, and different types can do different things.
+data types is the method to know what data type it is.
+
+python has following data type:
+ 
+1)text type : string
+2)numeric types: int, float, complex 
+3)sequence types:list, tuple,range
+4)mapping type:dict
+5)set type:set, forzenset
+6)boolean type: bool
+7)Binary types: bytes,bytearray,memoryview
+8)None type: none type.
+
+
+1]Strings:
+string in python are surrounded by either single or double quotation marks.
+multiline string to a variable by using three quotes.we can use three double quotes or three single quotes.
+
+*loop-
+looping through a string-we can loop through the characters in string with a for loop.
+example:loop through the letters in the word "banana":
+for x in "banana":
+   print(x)
+
+*string length-
+the length of string,use the len() function.
+example:the len() function return the length of string.
+a="hello, world!"
+print(len(a))
+
+String Method:
+1)capitalize(): it converts first character to upper case.
+2)casefold():converts string into lower case.
+3)center():returns a centered string.
+4)count():returns the number of times a specified value occurs in a string.
+5)encode():returns an encodede version of the string.
+6)endswith():returns true if string end with the specified value.
+7)expandtabs():set the tab size of the string.
+8)find():searches the string fora specified value and return the position of where it was found.
+9)format():formats specified values in string.
+10)format_map():formats specified value in string.
+11)index():the string for a specified value and returns the position of where it was found.
+12)isalnum():true if all charachter in the string are alphabet and number.
+13)isalpha():True if all characters in the string are alphabet.
+14)isdecimal():true if all character in the string are decimal.
+15)isdigit():True if all character in the string are digit.
+16)isidentifier():True if string is an identifier.
+17)iflower():True if all character in string are lower case
+18)isnumeric():true if all charcater in string are numeric.
+19)isprintable():True if all character in string are printable.
+20)isspace():True if all character in string are whitespace.
+21)istitle():true if the string follows the rules of title.
+22)isupper():True if alll character in string are upper case.
+23)join():join the elements of an iterable to the end of the string.
+24)just():a left justified versionof the string.
+25)lower():string convert into lower case.
+26)istrip():left trim version of the string.
+27)partition():a tuple where the string is parted into three parts.
+28)replace():string where a specified value is replaced with a specified value.
+29)rfind():string for a specified value and returns the last position of where it was found.
+30)split():splits the string at the specified separator and returms a list.
+31)startswith():true if the string starts with the specified value.
+32)strip():trimmed version of the string.
+33)swapcase():swapscases,lower case become upper case and vice versa.
+34)title():frist character convert into upper case.
+35)translate(): translate string.
+36)upper():converts a string into upper case.
+37)zfill():fill the string with a specified number of 0 values at the begning.
+
+
+
+ 
+
+
+
+
